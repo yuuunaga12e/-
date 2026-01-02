18dd085401e83f103aa1c7a93d0235a3773d14ea
yuuunaga12e <yuuunaga12e@users.noreply.github.com>
2026-01-02 13:41:17 +0900

    Publish config.js to allow business contact info on the web

---
+++ config.js
@@ -0,0 +1,13 @@
+// ==================================================
+// 【重要】このファイルには個人情報を記述します。
+// GitHub等にはアップロードされない設定（.gitignore）になっています。
+// ==================================================
+
+// ▼ 電話番号・メールアドレス
+const phoneNumber = "080-1726-0359";
+const emailAddress = "yuuunaga.12@gmail.com";
+
+// ▼ QRコードのリンク先URL
+const qrLineUrl = "https://lin.ee/RlVTD8p";
+const qrInstaUrl = "https://www.instagram.com/yukari_babysitter.fukuoka/";
+const qrHpUrl = "https://lynxbase.vercel.app/";
